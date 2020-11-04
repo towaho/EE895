24b68248b464434a507c9723ed7ade1a05e95107
jalr <mail@jalr.de>
2020-11-04 01:02:53 +0100

    initial commit

---
+++ src/EE895.h
@@ -0,0 +1,18 @@
+#ifndef __EE895_ARDUINO_LIBARARY_H__
+#define __EE895_ARDUINO_LIBARARY_H__
+
+#include "Arduino.h"
+#include <Wire.h>
+
+#define EE895_ADDRESS_MODBUS 0x5F
+
+
+class EE895 {
+  public:
+    EE895(void);
+
+
+  private:
+};
+
+#endif /* !__EE895_ARDUINO_LIBARARY_H__ */
